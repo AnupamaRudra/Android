0134ed741fc4b3fc15ebfa2c2f8e4c8cd3143330
AnupamaRudra <anupamarudra111@gmail.com>
2018-03-30 18:55:47 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.music;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	Button b1,b2;
+	MediaPlayer m1;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		b1=(Button) findViewById(R.id.button1);
+		b2=(Button) findViewById(R.id.button2);
+		m1=MediaPlayer.create(MainActivity.this,R.raw.abcd);
+		b1.setOnClickListener(new View.OnClickListener() {
+			
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				m1.start();	
+			}
+		});
+		b2.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				m1.pause();
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
